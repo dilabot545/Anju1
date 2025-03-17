a91c5edd14c42b9a871913349fe65542c84fc5dd
dilabot545 <samannawarathnanawarathna@gmail.com>
2025-03-17 06:53:41 +0530

    Update session.js

--- session.js
+++ session.js
@@ -43,5 +43,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~zBsAgJgL#jZ81ndko51shBZZ4-MVpr6yFAlt_rsZOeuDiTMFBIYI",
 SUDO: process.env.SUDO || ["94717775628","94758775628"],
 PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> 📥 ᵈᵒʷⁿˡᵒᵃᵈ ʷⁱᵗʰⁱⁿᵍ 14 ᵈᵃʸˢ.‼️                       .     ❕  *`✨️𝘿𝙆 𝙈𝙊𝙑𝙄𝙀𝙎✨️`*  ❕                |       *❖◉◎__[D̲̅][I̲̅][L̲̅][A̲̅]__◎◉❖*",
+FOOTER: process.env.FOOTER || "> 📥 ᵈᵒʷⁿˡᵒᵃᵈ ʷⁱᵗʰⁱⁿᵍ 14 ᵈᵃʸˢ.❗                                                               *`✨️𝘿𝙆 𝙈𝙊𝙑𝙄𝙀𝙎✨️`*                                                                                                                                                    *❖◉◎__[D̲̅][I̲̅][L̲̅][A̲̅]__◎◉❖*",
 };
